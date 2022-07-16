9e805f328487107c12e50e082ae9e57788d231ee
Christian Dean <christiandeandemesa@gmail.com>
2022-07-15 23:07:25 -0700

    filter functionality

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ class App extends Component {
 	constructor() {
 		super();
 
+		// Initializes the App's state.
 		this.state = {
-			// Initializes the App's state.
-			monsters: []
+			monsters: [],
+			searchField: ''
 		};
+
+		console.log('constructor');
 	}
 
+	// Executes after the component initializes below in render.
 	componentDidMount() {
-		// Executes after the component initializes below in render.
+		console.log('componentDidMount');
+
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(res => res.json())
 			.then(data =>
@@ -29,10 +34,35 @@ class App extends Component {
 			);
 	}
 
+	// When the input is changed, searchField changes to be the input's element's target's value, then sets it in its state using the shorthand for {searchField: searchField}.
+	onSearchChange = e => {
+		const searchField = e.target.value.toLocaleLowerCase();
+		this.setState(() => {
+			return {searchField};
+		});
+	};
+
+	// Executes after the component's constructor above.
 	render() {
+		console.log('render');
+
+		const {monsters, searchField} = this.state; // Destructures the keys from state.
+		const {onSearchChange} = this; // Destructures the function from this class component.
+		// filteredMonsters takes the monsters array in state, filters out each monster (i.e. element) that has the same string as the one in the searchField (i.e. input), then returns an array.
+		const filteredMonsters = monsters.filter(monster => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
+
 		return (
 			<div className='App'>
-				{this.state.monsters.map(monster => {
+				<input
+					type='search'
+					placeholder='Search for monsters'
+					className='search-box'
+					onChange={onSearchChange}
+				/>
+				{/* Maps over each monster in the filteredMonsters array, then creates an h1 element for each one */}
+				{filteredMonsters.map(monster => {
 					return <h1 key={monster.id}>{monster.name}</h1>;
 				})}
 			</div>
